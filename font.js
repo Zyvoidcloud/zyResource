2d0a19b964c919f8a00a2ce91f3869330c7b07d8
Zyvoidcloud <zyvoidcloud@gmail.com>
2025-06-15 01:25:31 +0700

    Create font.js

---
+++ font.js
@@ -0,0 +1,4 @@
+function loadGoogleFonts(){const links=[{rel:'preconnect',href:'https://fonts.googleapis.com'},{rel:'preconnect',href:'https://fonts.gstatic.com',crossOrigin:'crossorigin'},{rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Audiowide&family=Oxanium:wght@200..800&display=swap'}];links.forEach(link=>{const el=document.createElement('link');Object.assign(el,link);document.head.appendChild(el)})}
+const uF=`https://cdn.jsdelivr.net/gh/Zyvoidcloud/`;
+window.addEventListener('DOMContentLoaded',loadGoogleFonts);
+goDash="https://www.zyvoid.cloud/dashboard";
