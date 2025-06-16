07ec3d35f67a5d594e030352782f35969e92765d
Zyvoidcloud <zyvoidcloud@gmail.com>
2025-06-16 09:54:22 +0700

    Update font.js

--- font.js
+++ font.js
@@ -1,4 +1,19 @@
-function loadGoogleFonts(){const links=[{rel:'preconnect',href:'https://fonts.googleapis.com'},{rel:'preconnect',href:'https://fonts.gstatic.com',crossOrigin:'crossorigin'},{rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Audiowide&family=Oxanium:wght@200..800&display=swap'}];links.forEach(link=>{const el=document.createElement('link');Object.assign(el,link);document.head.appendChild(el)})}
-const uF=`https://cdn.jsdelivr.net/gh/Zyvoidcloud/`;
-window.addEventListener('DOMContentLoaded',loadGoogleFonts);
-goDash="https://www.zyvoid.cloud/dashboard";
+function loadGoogleFonts(){
+ const links=[{rel:'preconnect',href:'https://fonts.googleapis.com'},{rel:'preconnect',href:'https://fonts.gstatic.com',crossOrigin:'crossorigin'},{rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Audiowide&family=Oxanium:wght@200..800&display=swap'}];links.forEach(link=>{const el=document.createElement('link');Object.assign(el,link);document.head.appendChild(el)})}
+ window.addEventListener('DOMContentLoaded',loadGoogleFonts);
+ 
+const zyLink = {
+ Access: `zyAccess`,
+ goAccess: `https://www.zyvoid.cloud/2025/06/access.html`,
+ ads1: ``,
+ dashboard: `https://www.zyvoid.cloud/2025/06/dashboard.html`,
+ hero: `https://www.zyvoid.cloud/2025/06/hero.html`,
+ skin: `https://www.zyvoid.cloud/2025/06/skin.html`,
+ fileSkin: `https://cdn.jsdelivr.net/gh/Zyvoidcloud/`,
+ battleEffect: `https://www.zyvoid.cloud/2025/06/battleEffect.html`,
+ unlockFps: `https://www.zyvoid.cloud/2025/06/unlockFps.html`,
+ tutorial: ``,
+ information: `https://www.zyvoid.cloud/2025/06/informasi.html`,
+ youtube: `https://youtube.com/@zyvoid`,
+ tiktok: ``
+};
